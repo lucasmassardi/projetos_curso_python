46e6eba2800fd434df0f039c80ea3c9e4423c6ae
lucasmassardi <lucasmassardi@hotmail.com>
2023-11-16 17:23:35 -0300

    second commit

--- ecercicio_23.py
+++ ecercicio_23.py
@@ -2,7 +2,7 @@ n = int(input("Informe um número: "))
 u = n // 1 % 10
 d = n // 10 % 10
 c = n // 100 % 10
-m = n // 1000 % 10
+m = n // 1000 % 1
 
 print(f"Analisando o numero {n}")
 print(f"Unidade: {u}")
